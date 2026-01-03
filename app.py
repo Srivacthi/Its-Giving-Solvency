26d63cab76a2edfcc21e061048d0817ed3940115
Srivacthi <Srivacthis@outlook.com>
2026-01-02 19:24:28 -0500

    drag feature works!

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/old")
 def home():
     return render_template("budget.html")
+
+@app.route("/")
+def new_budget():
+    return render_template("budget2.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
