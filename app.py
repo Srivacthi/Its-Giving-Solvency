7b96010e22a79fc6c4294c2747b300b2139d00c2
Srivacthi <Srivacthis@outlook.com>
2026-01-08 00:47:11 -0500

    individual add on delete button and grey display buttons

--- app.py
+++ app.py
@@ -3,16 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/old")
-def home():
+def old_route():
     return render_template("budget.html")
 
 @app.route("/")
-def old_budget():
+def home():
     return render_template("budget2.html")
-
-@app.route("/new")
-def new_budget():
-    return render_template("budget3.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
